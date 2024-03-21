916a29a4decbe91d987d0f230f96f8daa15cdc91
y-sakamoto-9127 <158486236+y-sakamoto-9127@users.noreply.github.com>
2024-03-21 23:51:19 +0900

    kadai_005再提出
    定数→変数、計算結果も変数に入れるように修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,10 @@
-// 定数の定義・値の代入
-const weight = 68;
-const height = 1.7;
+// 変数の定義・値の代入
+let weight = 68;
+let height = 1.7;
+let bmi = 0;
+
+// 計算
+bmi = weight / (height * height)
 
 // 計算結果表示
-console.log(weight / (height * height));
+console.log(bmi);
